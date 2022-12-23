647a88ccf411f2117a6d08c526c606f08738172d
John DeSilva <desilvjo@umich.edu>
2022-12-23 13:36:18 -0500

    Implement exe method using quick_openai

--- lib/openai_pipe.rb
+++ lib/openai_pipe.rb
@@ -2,10 +2,18 @@
 
 require_relative "openai_pipe/version"
 
+require "quick_openai"
+
 module OpenAIPipe
   class Error < StandardError; end
 
   def self.exe
-    puts 'Hello World!'
+    input_from_pipe = $stdin.read if $stdin.stat.pipe?
+
+    input_from_arguments = ARGV.join(" ") if ARGV.any?
+
+    prompt = [input_from_arguments, input_from_pipe].compact.join("\n\n")
+
+    puts prompt.gpt3
   end
 end
